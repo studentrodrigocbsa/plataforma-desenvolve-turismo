dfabe0ee1047118182554776e0dfe66a452b85e2
= <rcbsa@protonmail.com>
2025-02-20 09:31:49 -0300

    Ngrok config + vite
    deploy

---
+++ app/forms/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: true, // Permite que o Vite seja acessível por outros dispositivos
+    port: 5173, // Porta padrão do Vite, altere se necessário
+    strictPort: true, // Garante que a porta escolhida será usada
+    allowedHosts: ["05c7-177-55-205-120.ngrok-free.app"], // Define a origem permitida
+    cors: true, // Habilita CORS
+    hmr: {
+      clientPort: 443, // Necessário para funcionar corretamente com ngrok
+    }
+  }
+});
