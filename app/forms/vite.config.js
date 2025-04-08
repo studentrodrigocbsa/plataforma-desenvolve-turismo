3ca4007a8e6892b39d168945c9c34f4062f30d7a
= <rcbsa@protonmail.com>
2025-04-08 15:51:47 -0300

    build configs

--- app/forms/vite.config.js
+++ app/forms/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import { DOMINIO } from './front/app/infra/domain';
 
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
   server: {
     proxy: {
       '/api': {
@@ -11,12 +16,25 @@ export default defineConfig({
       },
     },
     host: true, // Permite que o Vite seja acessível por outros dispositivos
-    port: 5173, // Porta padrão do Vite, altere se necessário
+    port: 5174, // Porta padrão do Vite, altere se necessário
     strictPort: true, // Garante que a porta escolhida será usada
     allowedHosts: [DOMINIO.replace('https://','')], // Domínio
     cors: true, // Habilita CORS
     hmr: {
       clientPort: 443, // Necessário para funcionar corretamente com ngrok (?)
+    }
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        dados: './front/pages/dados.html',
+        dashboard: './front/pages/dashboard.html',
+        login: './front/pages/login.html',
+        relatorio: './front/pages/relatorio.html',
+        survey: './front/pages/survey.html',
+      }
     }
   }
 });
