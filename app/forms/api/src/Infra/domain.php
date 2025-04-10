9376e9c52ddf0aa63bbd8071fb74268392b2246d
= <rcbsa@protonmail.com>
2025-04-10 16:59:00 -0300

    dominio

--- app/forms/api/src/Infra/domain.php
+++ app/forms/api/src/Infra/domain.php
@@ -1,3 +1,3 @@
 <?php
 //const DOMINIO = 'http://localhost:5174';
-const DOMINIO = 'https://ready-summary-quail.ngrok-free.app';
+const DOMINIO = 'https://bsi.cefet-rj.br/masa/';
--- app/forms/front/app/infra/domain.ts
+++ app/forms/front/app/infra/domain.ts
@@ -1,2 +1,2 @@
 //export const DOMINIO = 'http://localhost:5174';
-export const DOMINIO = 'https://ready-summary-quail.ngrok-free.app';
+export const DOMINIO = 'https://bsi.cefet-rj.br/masa/';
